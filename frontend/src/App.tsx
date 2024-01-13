e73752f8ee705d8ee368928dcc86f29a5f6e705c
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-01-13 18:14:14 -0300

    feat: adicionando listagem de produtos no frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,21 @@ import { api } from "./api";
 
 import './App.css';
 
+interface Product {
+  id: string,
+  code: string,
+  name: string,
+  description: string,
+  price: number
+}
+
 function App() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   async function fetchProducts() {
-    setProducts(await api.get("product"));
+    const res = await api.get("product");
+
+    setProducts(res.data);
   }
 
   useEffect(() => {
@@ -18,6 +28,18 @@ function App() {
   return (
     <main className="App">
       <button>Adicionar Produto</button>
+      {
+        products.length ?
+        products.map(product => (
+          <li key={product.id}>
+            <span>{product.code}</span>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <span>{product.price}</span>
+          </li>
+        )) :
+        <p>Nenhum produto registrado</p>
+      }
     </main>
   );
 }
