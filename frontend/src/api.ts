74e0d87a9c3aa63286368fa81983a13dcebfca71
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-10 13:59:54 +0000

    fix: feita URL da API incluir protocolo

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_API_URL || "localhost:3001"
+const url = process.env.REACT_APP_API_URL || "http://localhost:3001"
 
 export const api = axios.create({
-  baseURL: `http://${url}/api`
+  baseURL: `${url}/api`
 });
